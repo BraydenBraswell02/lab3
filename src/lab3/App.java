2cafca0409df721617902e300d6b0f8facf78c7f
Brayden Braswell <braydenbraswell@Braydens-MacBook-Pro.local>
2026-08-31 09:02:03 -0500

    main

---
+++ src/lab3/App.java
@@ -0,0 +1,12 @@
+package lab3;
+
+public class App implements Runnable {
+  
+  public void init() {
+    
+  }
+
+  public void run() {
+
+  }
+}
---
+++ src/lab3/Main.java
@@ -0,0 +1,13 @@
+package lab3;
+
+public class Main {
+
+  public static App app = new App();
+
+  public static void main(String args[]) {
+
+    app.init();
+
+    app.run();
+  }
+}
